1e6a8f0275824f1df6bee45699de746c74e21f83
rodrigobell <rbell594@gmail.com>
2017-01-18 15:00:19 -0800

    Added more structure and styles to site and refractored styles into several files

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
-import './styles.css';
+import './styles/index.css';
+import './styles/navbar.css';
+import './styles/homeSection.css';
+import './styles/aboutSection.css';
+import './styles/projectsSection.css';
+import './styles/footer.css';
 
 $(window).on("load resize", function () {
     $(".fill-screen").css("height", window.innerHeight);
 });
-
-
