708544179fd0bf5b0afa319774bc919bc9e500ec
rodrigobell <rbell594@gmail.com>
2017-01-18 14:41:40 -0800

    Added more structure and styles to html and css files

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -19,9 +19,10 @@ export default {
     ],
     module: {
         loaders: [
+            {test: /\.html$/, exclude: /node_modules/, loaders: ['html']},
             {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
             {test: /\.css$/, exclude: /node_modules/, loaders: ['style', 'css', 'autoprefixer']},
-            {test: /\.(png|jpg|jpeg|ttf|eot)$/, exclude: /node_modules/, loaders: ['url?limit=5000']}
+            {test: /\.(png|jpg|jpeg|svg|ttf|eot)$/, exclude: /node_modules/, loaders: ['url?limit=5000']}
         ]
     }
 }
