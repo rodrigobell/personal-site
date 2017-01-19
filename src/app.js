cff079de511bb8ae4f62cbabe03a3b43a07326ea
RB <RB>
2017-01-19 10:30:48 -0800

    Worked on styling and structure for home section

--- src/app.js
+++ src/app.js
@@ -8,3 +8,17 @@ import './styles/footer.css';
 $(window).on("load resize", function () {
     $("#home, #about, #projects").css("height", window.innerHeight);
 });
+
+const homeWords = $(".home-words");
+const homeQuotes = $(".home-quotes");
+
+for (let i = 0; i < homeWords.length; i++) {
+    $(homeWords[i]).on({
+        mouseenter: function() {
+            $(homeQuotes[i]).removeClass("hidden");
+        },
+        mouseleave: function() {
+            $(homeQuotes[i]).addClass("hidden");
+        }
+    });
+}
