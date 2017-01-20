f46093fdb01361048cb43897cf7e22b6dcb1f437
rodrigobell <rbell594@gmail.com>
2017-01-20 14:54:59 -0800

    Changed home image to golden globe; Added smooth scrolling to navbar links

--- src/app.js
+++ src/app.js
@@ -5,20 +5,31 @@ import './styles/sections/about.css';
 import './styles/sections/projects.css';
 import './styles/footer.css';
 
-$(window).on("load resize", function () {
-    $("#home, #about, #projects").css("height", window.innerHeight);
+
+// Resize each section to be height of window viewport
+$(window).on('load resize', function () {
+    $('#home, #about, #projects').css('height', window.innerHeight);
 });
 
-const homeWords = $(".home-words");
-const homeQuotes = $(".home-quotes");
+// Smooth scrolling from navbar to page section
+$('nav a').on('click', function() {
+   $('html, body').animate({
+           scrollTop: $($(this).attr('href')).offset().top
+       }, 1500, 'swing');
+   event.preventDefault();
+});
+
+// Hover handler for home section quotes
+const homeWords = $('.home-words');
+const homeQuotes = $('.home-quotes');
 
 for (let i = 0; i < homeWords.length; i++) {
     $(homeWords[i]).on({
         mouseenter: function() {
-            $(homeQuotes[i]).removeClass("hidden");
+            $(homeQuotes[i]).removeClass('hidden');
         },
         mouseleave: function() {
-            $(homeQuotes[i]).addClass("hidden");
+            $(homeQuotes[i]).addClass('hidden');
         }
     });
 }
