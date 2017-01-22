51bf51fd3ebfb817f5276b24ec20530732e90ec7
rodrigobell <rbell594@gmail.com>
2017-01-22 13:01:36 -0800

    Added parallax.js scene to home

--- src/app.js
+++ src/app.js
@@ -5,14 +5,14 @@ import './styles/sections/about.css';
 import './styles/sections/projects.css';
 import './styles/footer.css';
 import mixitup from './mixitup.min';
-
+import parallax from './jquery.parallax.min';
 
 // Resize each section to be height of window viewport
 $(window).on('load resize', function () {
     $('#home, #about, #projects').css('height', window.innerHeight);
 });
 
-// Smooth scrolling from navbar to page section
+// Smooth scrolling from navbar link to corresponding section
 $('nav a').on('click', function() {
    $('html, body').animate({
            scrollTop: $($(this).attr('href')).offset().top
@@ -20,13 +20,20 @@ $('nav a').on('click', function() {
    event.preventDefault();
 });
 
+// Initialize parallax effect in home section
+$('#scene').parallax({
+    clipRelativeInput: true
+});
+
 // Handle hover event on home section words to show and hide quotes
-const homeWords = $('#home-words p');
+const sceneWords = $('#scene .layer p');
 const homeQuotes = $('#home-quotes blockquote');
 
+console.log(sceneWords);
+console.log(homeQuotes);
 
-for (let i = 0; i < homeWords.length; i++) {
-    $(homeWords[i]).on({
+for (let i = 0; i < sceneWords.length; i++) {
+    $(sceneWords[i]).on({
         mouseenter: function() {
             $(homeQuotes[i]).removeClass('hidden');
         },
